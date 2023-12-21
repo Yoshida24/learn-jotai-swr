877b57935f32731c7460bd679ffba24305068f85
Yoshida24 <trihead1.dev@gmail.com>
2023-12-21 09:55:32 +0900

    chore: refactor directory structure

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,10 +1,30 @@
-import Count from "./Count";
-import Plus from "./Plus";
-import Minus from "./Minus";
+import "./style.css";
+import { atom, useAtom } from "jotai";
+
+const countAtom = atom(0);
+
+function Minus() {
+  const [, setCount] = useAtom(countAtom);
+  return <button onClick={() => setCount((count) => count - 1)}>-</button>;
+}
+
+function Plus() {
+  const [, setCount] = useAtom(countAtom);
+  return <button onClick={() => setCount((count) => count + 1)}>+</button>;
+}
+
+function Count() {
+  const [count] = useAtom(countAtom);
+  return (
+    <>
+      <p>count is {count}</p>
+    </>
+  );
+}
 
 function Counter() {
   return (
-    <div className="container">
+    <div>
       <Count />
       <Minus />
       <Plus />
